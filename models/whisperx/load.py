26538d5d8ae060b59150c8bf6a39c1d0bb43bce4
지유민 <yuminji@stunitas.com>
2024-12-31 15:18:57 +0900

    init

--- models/whisperx/load.py
+++ models/whisperx/load.py
@@ -1,6 +1,9 @@
-import whisperx
 import json
 from tools.cli import cli
+
+from models.whisperx.asr import load_model
+from models.whisperx.alignment import load_align_model, align
+from models.whisperx.audio import load_audio
 
 
 def whisper_result(audio_path, language='ko', api_keys='./assets/api_key.json'):
@@ -17,13 +20,13 @@ def whisper_result(audio_path, language='ko', api_keys='./assets/api_key.json'):
     transcribe_options = dict(task="transcribe", **options)
 
     # whisperx reference model inference
-    model = whisperx.load_model("deepdml/faster-whisper-large-v3-turbo-ct2", device=args.device, device_index=list(range(2)), compute_type=args.compute_type)
-    audio = whisperx.load_audio(audio_path)
+    model = load_model("deepdml/faster-whisper-large-v3-turbo-ct2", device=args.device, device_index=list(range(2)), compute_type=args.compute_type)
+    audio = load_audio(audio_path)
 
     # Align whisperx output
     result = model.transcribe(audio, batch_size=args.batch_size)
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=args.device)
-    result = whisperx.align(result["segments"], model_a, metadata, audio, device=args.device, return_char_alignments=False)
+    model_a, metadata = load_align_model(language_code=result["language"], device=args.device)
+    result = align(result["segments"], model_a, metadata, audio, device=args.device, return_char_alignments=False)
 
     # if you want to speaker diarization, uncomment the following lines
     '''
