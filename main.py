eb1cb9f430392bec0a1612adb4c034020fac4cc1
지유민 <yuminji@stunitas.com>
2024-12-31 14:46:36 +0900

    init

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 
 from tools.cli import cli
 from tools.transcript import generate_subtitles, matching_formats
-from tools.utils import extract_audio, transform, logging_print
+from tools.utils import extract_audio, transform, logging_print, save_json
 from tools.visualize import visualizer
 from models.whisperx.load import whisper_result
 
@@ -19,6 +19,9 @@ def main():
 
         whispers = whisper_result(args.input_path)
         logging_print("Whisper results obtained.")
+
+        save_json(args.input_path, whispers)
+        logging_print("Json 저장 완료.")  # 성공 메시지를 로그에 기록
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")  # Logging any unexpected error
