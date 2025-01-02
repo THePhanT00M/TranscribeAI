0fa48285ae4a4aabfa178589e0bb783ba79fa574
지유민 <yuminji@stunitas.com>
2025-01-02 11:29:18 +0900

    init

--- tools/utils.py
+++ tools/utils.py
@@ -69,17 +69,40 @@ def get_duration(audio_path):
 
 
 
-def extract_audio(input_file, output_file = './dataset/audio/extracted_audio.mp3'):
-    
+def extract_audio(input_file, output_dir='./dataset/audio'):
+    """
+    주어진 비디오 파일에서 오디오를 추출하여 mp3 파일로 저장합니다.
+
+    매개변수:
+    input_file (str): 오디오를 추출할 비디오 파일의 경로
+    output_file (str, optional): 추출된 오디오를 저장할 파일의 경로. 기본값은 './dataset/audio/extracted_audio.mp3'
+
+    반환값:
+    str: 추출된 오디오 파일의 경로
+    """
+
+    # 입력 파일명에서 확장자 제거하고, 출력 파일명 생성
+    input_filename = os.path.splitext(os.path.basename(input_file))[0]
+    output_file = os.path.join(output_dir, f'{input_filename}.mp3')
+
+    # 출력 파일의 디렉토리를 생성
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # ffmpeg 명령어를 구성하는 리스트
     cmd = [
-        'ffmpeg',
-        '-i', input_file,
-        '-vn',
-        output_file
+        'ffmpeg',  # ffmpeg 프로그램
+        '-i', input_file,  # 입력 파일을 지정
+        '-vn',  # 비디오 트랙을 사용하지 않음 (오디오만 추출)
+        output_file  # 출력 파일 경로를 지정
     ]
-    
+
+    # subprocess.run() 함수를 사용하여 ffmpeg 명령어를 실행
+    # input='y\n'은 어떤 사용자 입력이 필요할 때 'y'를 입력하도록 함
+    # text=True는 입력 및 출력이 문자열 형식임을 지정
     subprocess.run(cmd, input='y\n', text=True)
 
+    # 추출된 오디오 파일의 경로를 반환
     return output_file
 
 
